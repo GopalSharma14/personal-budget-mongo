3dbcd344dbbc37fbf59a9c7aa020abb263edf5a8
GopalSharma14 <cse.gopalsharma@gmail.com>
2020-09-29 13:20:41 -0400

    Loading data from json file

--- server.js
+++ server.js
@@ -2,31 +2,53 @@ const { publicEncrypt } = require('crypto');
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
 
 app.use('/', express.static('public'));
 
-const  budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget:70
-    },
-    {
-        title: 'Rent',
-        budget:330
-    },
-    {
-        title: 'Groceries',
-        budget:100
-    }
-]};
+// const  budget = {
+//     myBudget: [
+//     {
+//         title: 'Eat out',
+//         budget:70
+//     },
+//     {
+//         title: 'Rent',
+//         budget:330
+//     },
+//     {
+//         title: 'Groceries',
+//         budget:100
+//     },
+//     {
+//         title: 'Internet',
+//         budget:100
+//     },
+//     {
+//         title: 'Laundry',
+//         budget:100
+//     },
+//     {
+//         title: 'Groceries',
+//         budget:100
+//     },
+//     {
+//         title: 'Groceries',
+//         budget:100
+//     }
+
+// ]};
 
 app.get('/hello',(req, res) => {
     res.send('Hello world!');
 });
 
+// app.get('/budget',(req, res) => {
+//     res.json(budget);
+// });
 app.get('/budget',(req, res) => {
-    res.json(budget);
+    res.sendFile(path.normalize(__dirname + '/myfile.json'));
 });
 app.listen(port, () =>{
     console.log(`Example app listening at http://localhost:${port}`)
